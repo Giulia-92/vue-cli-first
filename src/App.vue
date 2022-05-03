a1baede6b0ac45a397c663080e30c8d6e8a25357
Giulia-92 <CIONCO.GIULIA@GMAIL.COM>
2022-05-03 17:45:50 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <NuovoTitolo />
+  </div>
+</template>
+
+<script>
+import NuovoTitolo from './components/NuovoTitolo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NuovoTitolo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/NuovoTitolo.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+
+        <h1>La mia prima app con CLI!!</h1>
+        <img alt="Vue logo" src="../assets/logo.png">
+
+    </div>    
+</template>
+
+<script>
+export default {
+    name:'NuovoTitolo'
+}
+</script>
