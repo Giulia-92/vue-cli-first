a9371e04fa34a6516eeee6caa4f90aba0159cca7
Giulia-92 <CIONCO.GIULIA@GMAIL.COM>
2022-05-03 17:57:37 +0200

    vue-cli-first

--- src/components/NuovoTitolo.vue
+++ src/components/NuovoTitolo.vue
@@ -3,7 +3,7 @@
 
         <h1>La mia prima app con CLI!!!</h1>
         <img alt="Vue logo" src="../assets/logo.png">
-
+    <img alt="jumbo" src="../assets/jumbo.png">
     </div>    
 </template>
 
